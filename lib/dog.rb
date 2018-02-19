7b496cbccbf6c9f25a1371301346f42ecc4612b6
Dylain Cohen <dylain.cohen@wework.com>
2018-02-19 23:20:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 
 class Dog
 
-  def initialize(name)
-    @name = name
+
+
+  def name=(new_name)
+    @name = new_name
   end
 
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
+
 
 end
 
