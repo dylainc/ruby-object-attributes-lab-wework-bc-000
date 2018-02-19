9b0ab45892325a5d466ee0948146aa884342a514
Dylain Cohen <dylain.cohen@wework.com>
2018-02-19 23:13:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@
 class Dog
 
 
-  def initialize(name)
-    @name = name
-  end
+  def name=(dogs_name)
+     @this_dogs_name = dogs_name
+   end
 
-  def name
-    @name
-  end
+   def name
+     @this_dogs_name
+   end
 
 
 
