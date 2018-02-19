4d06c429939ce0c8c8b68d6a453679d12bedda8c
Dylain Cohen <dylain.cohen@wework.com>
2018-02-19 23:06:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,12 +15,16 @@ class Dog
 
 
 
-  def breed= dog_breed
+  initialize(dog_breed)
     @this_dogs_breed = dog_breed
   end
 
   def breed
     @this_dogs_breed
+  end
+
+  def breed= new_dog_breed
+    @this_dogs_breed = new_dog_breed
   end
 
 end
